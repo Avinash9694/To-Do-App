7dd4df9ffa7cd0319087df0d9b764c5b040fa28e
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2023-08-16 23:39:18 +0530

    added functionality for editing existing task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,30 +1,46 @@
 import React from "react";
 
-const AddTask = ({ tasklist, setTasklist }) => {
+const AddTask = ({ tasklist, setTasklist, task, setTask }) => {
   function handleSubmit(e) {
     e.preventDefault();
 
-    const date = new Date();
-
-    const newTask = {
-      id: date.getTime(),
-      name: e.target.task.value,
-      time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
-    };
-    setTasklist([...tasklist, newTask]);
-    e.target.task.value = "";
+    if (task.id) {
+      const date = new Date();
+      const updatedTasklist = tasklist.map((todo) =>
+        todo.id === task.id
+          ? {
+              id: task.id,
+              name: task.name,
+              time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+            }
+          : todo
+      );
+      setTasklist(updatedTasklist);
+      setTask({});
+    } else {
+      const date = new Date();
+      const newTask = {
+        id: date.getTime(),
+        name: e.target.task.value,
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+      };
+      setTasklist([...tasklist, newTask]);
+      setTask({});
+    }
   }
 
   return (
     <section className="addTask">
       <form onSubmit={handleSubmit}>
         <input
-          type="task"
+          type="text"
           name="task"
+          value={task.name || ""}
           autoComplete="off"
           placeholder="Add Task"
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
         ></input>
-        <button type="submit">Add</button>
+        <button type="submit">{task.id ? "Update" : "Add"}</button>
       </form>
     </section>
   );
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,6 +1,15 @@
 import React from "react";
 
-const ShowTask = ({ tasklist, setTasklist }) => {
+const ShowTask = ({ tasklist, setTasklist, task, setTask }) => {
+  function handleEdit(id) {
+    const selectedTask = tasklist.find((item) => item.id === id);
+    setTask(selectedTask);
+  }
+
+  function handleDelete(id) {
+    const newList = tasklist.filter((item) => item.id !== id);
+    setTasklist(newList);
+  }
   return (
     <section className="showTask">
       <p className="head">
@@ -8,19 +17,29 @@ const ShowTask = ({ tasklist, setTasklist }) => {
           <span className="title">Todo</span>
           <span className="count">{tasklist.length}</span>
         </span>
-        <span className="clearAll" onClick={() => setTasklist([])}>
+
+        <span onClick={() => setTasklist([])} className="clearAll">
           Clear All
         </span>
       </p>
+
       <ul>
-        {tasklist.map((task) => (
-          <li key={task.id}>
+        {tasklist.map((todo) => (
+          <li key={todo.id}>
             <p>
-              <span className="name">{task.name}</span>
-              <span className="time">{task.time}</span>
+              <span className="name">{todo.name}</span>
+              <span className="time">{todo.time}</span>
             </p>
-            <i className="bi bi-pencil-square"></i>
-            <i className="bi bi-trash"></i>
+
+            <i
+              onClick={() => handleEdit(todo.id)}
+              className="bi bi-pencil-square"
+            ></i>
+
+            <i
+              onClick={() => handleDelete(todo.id)}
+              className="bi bi-trash"
+            ></i>
           </li>
         ))}
       </ul>
