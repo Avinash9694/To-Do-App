3d1373bc6750b54d10aaeead22fe0e721eded959
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2023-08-16 13:32:43 +0530

    Added basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Header from "./components/Header.js";
+import AddTask from "./components/AddTask.js";
+import ShowTask from "./components/ShowTask.js";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <AddTask />
+      <ShowTask />
+    </div>
+  );
 }
 
 export default App;
