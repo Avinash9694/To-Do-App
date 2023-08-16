11e33974429d98e7f6a26a00a7f6741799d2f3d2
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2023-08-16 14:20:33 +0530

    Added header part. Functionality not added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
 import React from "react";
+import logo from "../assests/logo.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+        <span>Todo App</span>
+      </div>
+      <div className="themeSelector">
+        <span className="light"></span>
+        <span className="medium"></span>
+        <span className="dark"></span>
+        <span className="gradientOne"></span>
+        <span className="gradientTwo"></span>
+        <span className="gradientThree"></span>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
