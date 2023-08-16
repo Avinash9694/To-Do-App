e3f39ddbd7e140c7278c4ae5c6d894b7c55268ce
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2023-08-16 16:23:02 +0530

    added basic structure for showTask component

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const ShowTask = () => {
-  return <div>ShowTask</div>;
+  return (
+    <section className="showTask">
+      <p className="head">
+        <span>
+          <span className="title">Todo</span>
+          <span className="count">0</span>
+        </span>
+        <span className="clearAll">Clear All</span>
+      </p>
+    </section>
+  );
 };
 
 export default ShowTask;
